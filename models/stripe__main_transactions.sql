9baf38b3bfabf533dd59f1a0e02972d1e6e13baf
Shubham Pandey <shubhampcpandey13@gmail.com>
2022-06-02 13:07:59 +0530

    run sqlfluff fix on models

--- models/stripe__main_transactions.sql
+++ models/stripe__main_transactions.sql
@@ -1,110 +1,82 @@
-with balance_transaction as (
+with balance_transaction as ( select * from {{ var('balance_transaction') }} ),
 
-	select *
-	from {{ var('balance_transaction') }}
+charge as ( select * from {{ var('charge')}} ),
 
-), charge as (
+customer as ( select * from {{ var('customer')}} ),
 
-	select *
-	from {{ var('charge')}}
+subscription as ( select * from {{ var('subscription') }} ),
 
-), customer as (
+invoice as ( select * from {{ var('invoice') }} ),
 
-	select *
-	from {{ var('customer')}}
-), subscription as (
+invoice_line_item as ( select * from {{ var('invoice_line_item') }} ),
 
-	select *
-	from {{ var('subscription') }}
-), invoice as (
+plan as ( select * from {{ var('plan') }} ),
 
-	select *
-	from {{ var('invoice') }}
-), invoice_line_item as (
+product as ( select * from {{ source('fivetran_stripe', 'product') }} ),
 
-	select *
-	from {{ var('invoice_line_item') }}
-), plan as (
+refund as ( select * from {{ var('refund')}} ),
 
-	select *
-	from {{ var('plan') }}
-), product as (
-
-	select *
-	from {{ source('fivetran_stripe', 'product') }}
-), refund as (
-
-	select *
-	from {{ var('refund')}}
-),
-invoice_details as (
-  select
+invoice_details as ( select
     invoice.invoice_id,
     invoice.created_at,
     invoice.status,
     invoice_line_item.subscription_id as subscription_id,
+    product.name as product_name,
     DATE(invoice_line_item.period_start, 'UTC') as period_start,
-    DATE(invoice_line_item.period_end, 'UTC') as period_end,
-    product.name as product_name
-  from invoice_line_item
-  join invoice
-    on invoice.invoice_id = invoice_line_item.invoice_id
-  join plan
-    on invoice_line_item.plan_id = plan.plan_id
-  join product
-    on plan.product_id = product.id
-), grouped_line_items as (
+    DATE(invoice_line_item.period_end, 'UTC') as period_end
+    from invoice_line_item inner join invoice on invoice.invoice_id = invoice_line_item.invoice_id inner join plan on invoice_line_item.plan_id = plan.plan_id inner join product on plan.product_id = product.id ),
 
-  select
+grouped_line_items as ( select
     invoice_id,
     created_at as invoice_created_at,
     status as invoice_status,
     subscription_id as subscription_id,
     product_name,
-    min(period_start) as min_line_item_period_start,
-  from invoice_details
-  group by 1,2,3,4,5
-)
+    MIN(period_start) as min_line_item_period_start
+    from invoice_details group by 1, 2, 3, 4, 5 )
 
 select
-	subscription.subscription_id,
-	customer.customer_id,
-	customer.description as customer_description,
-	customer.email as customer_email,
-	subscription.status as subscription_status,
-	subscription.billing,
-	subscription.billing_cycle_anchor,
-	subscription.created_at as subscription_created_at,
-	subscription.cancel_at as subscription_cancel_at,
-	subscription.canceled_at as subscription_canceled_at,
-	subscription.ended_at as subscription_ended_at,
-	subscription.current_period_start,
-	subscription.current_period_end,
-	grouped_line_items.invoice_id,
-	grouped_line_items.invoice_created_at,
-	grouped_line_items.invoice_status,
-	grouped_line_items.min_line_item_period_start,
-	grouped_line_items.product_name,
-	balance_transaction.balance_transaction_id,
-	balance_transaction.created_at,
-	balance_transaction.available_on,
-	balance_transaction.currency,
-	balance_transaction.description as balance_transaction_description,
-	balance_transaction.amount/100 as balance_transaction_amount,
-	coalesce(charge.amount_refunded, 0)/100 as refund_amount,
-	balance_transaction.fee/100 as balance_transaction_fee,
-	balance_transaction.net/100 as balance_transaction_net,
-	balance_transaction.type as balance_transaction_type,
-	refund.metadata as refund_metadata
-
-from balance_transaction
-left join refund
-  on balance_transaction.balance_transaction_id=refund.balance_transaction_id
-left join charge
-  on charge.balance_transaction_id = balance_transaction.balance_transaction_id
-left join customer
-  on charge.customer_id = customer.customer_id
-left join grouped_line_items
-  on charge.invoice_id = grouped_line_items.invoice_id
-left join subscription
-  on grouped_line_items.subscription_id = subscription.subscription_id
+    subscription.subscription_id,
+    customer.customer_id,
+    customer.description as customer_description,
+    customer.email as customer_email,
+    subscription.status as subscription_status,
+    subscription.billing,
+    subscription.billing_cycle_anchor,
+    subscription.created_at as subscription_created_at,
+    subscription.cancel_at as subscription_cancel_at,
+    subscription.canceled_at as subscription_canceled_at,
+    subscription.ended_at as subscription_ended_at,
+    subscription.current_period_start,
+    subscription.current_period_end,
+    grouped_line_items.invoice_id,
+    grouped_line_items.invoice_created_at,
+    grouped_line_items.invoice_status,
+    grouped_line_items.min_line_item_period_start,
+    grouped_line_items.product_name,
+    balance_transaction.balance_transaction_id,
+    balance_transaction.created_at,
+    balance_transaction.available_on,
+    balance_transaction.currency,
+    balance_transaction.description as balance_transaction_description,
+    balance_transaction.type as balance_transaction_type,
+    refund.metadata as refund_metadata,
+    balance_transaction.amount / 100 as balance_transaction_amount,
+    COALESCE(charge.amount_refunded, 0) / 100 as refund_amount,
+    balance_transaction.fee / 100 as balance_transaction_fee,
+    balance_transaction.net / 100 as balance_transaction_net
+from
+    balance_transaction
+left join
+    refund on
+        balance_transaction.balance_transaction_id = refund.balance_transaction_id
+left join
+    charge on
+        charge.balance_transaction_id = balance_transaction.balance_transaction_id
+left join
+    customer on charge.customer_id = customer.customer_id
+left join
+    grouped_line_items on charge.invoice_id = grouped_line_items.invoice_id
+left join
+    subscription on
+        grouped_line_items.subscription_id = subscription.subscription_id
--- models/stripe__platform_fees.sql
+++ models/stripe__platform_fees.sql
@@ -1,4 +1,5 @@
-select *
-from {{ ref('stripe__main_transactions') }}
-where
-	balance_transaction_type = 'stripe_fee'
+select
+    *
+from
+    {{ ref('stripe__main_transactions') }}
+where balance_transaction_type = 'stripe_fee'
