4e2d62af3fba98e32c5432e6b2aa6c629cbeec31
Aryan Verma <aryanverma@Aryans-MacBook-Air.local>
2022-09-21 15:19:11 +0530

    update to dbt_metrics_0.3.2

--- models/metrics/stripe__monthly_bookings.sql
+++ models/metrics/stripe__monthly_bookings.sql
@@ -1,8 +1,8 @@
 -- depends_on: {{ ref('stripe__main_transactions') }}
 
 select *
-from {{ metrics.metric(
-    metric_name='stripe__monthly_bookings',
+from {{ metrics.calculate(
+    metric('stripe__monthly_bookings'),
     grain='month',
     dimensions=[],
     secondary_calculations=[]
--- models/metrics/stripe__monthly_platform_fees.sql
+++ models/metrics/stripe__monthly_platform_fees.sql
@@ -1,8 +1,8 @@
 -- depends_on: {{ ref('stripe__main_transactions') }}
 
 select *
-from {{ metrics.metric(
-	metric_name='stripe__monthly_platform_fees',
+from {{ metrics.calculate(
+	metric('stripe__monthly_platform_fees'),
 	grain='month',
 	dimensions=[],
 	secondary_calculations=[]
